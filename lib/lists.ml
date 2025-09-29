fa02e8dc6473abd97202d178150fec96aa690ade
Peter Schaedler <pwschaedler@gmail.com>
2025-09-28 17:53:27 -0700

    feat: implement at

--- lib/lists.ml
+++ lib/lists.ml
@@ -18,3 +18,20 @@ let rec last_two = function
 
 let%test _ = last_two [ "a"; "b"; "c"; "d" ] = Some ("c", "d")
 let%test _ = last_two [ "a" ] = None
+
+(* N'th Element of a List *)
+let rec at idx l =
+  match (idx, l) with
+  | i, _ when i < 0 -> None
+  | _, [] -> None
+  | 0, x :: _ -> Some x
+  | _, _ :: xs -> at (idx - 1) xs
+
+(* Alternative syntax for dealing with the list and an extra argument:
+   let rec at k = function ... *)
+(* Official solution was similar but used an if expression:
+   ...
+   | h :: t -> if k = 0 then Some h else at (k - 1) t *)
+
+let%test _ = at 2 [ "a"; "b"; "c"; "d"; "e" ] = Some "c"
+let%test _ = at 2 [ "a" ] = None
