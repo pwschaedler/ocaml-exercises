2375ce421414b799456fe1f369c3d8537bc4f0c8
Peter Schaedler <pwschaedler@gmail.com>
2025-09-28 17:13:30 -0700

    fix: formatting changes

--- lib/lists.ml
+++ lib/lists.ml
@@ -1,6 +1,9 @@
 (* Tail of a List *)
 let rec last (l : 'a list) : 'a option =
-  match l with [] -> None | [ x ] -> Some x | _ :: xs -> last xs
+  match l with
+  | [] -> None
+  | [ x ] -> Some x
+  | _ :: xs -> last xs
 
 let%test _ = last [ "a"; "b"; "c"; "d" ] = Some "d"
 let%test _ = last [] = None
