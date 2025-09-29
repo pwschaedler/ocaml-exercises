e645476810fa578d75e88f33275ba294dcf712a2
Peter Schaedler <pwschaedler@gmail.com>
2025-09-28 17:37:17 -0700

    feat: implement last_two

--- lib/lists.ml
+++ lib/lists.ml
@@ -7,3 +7,14 @@ let rec last (l : 'a list) : 'a option =
 
 let%test _ = last [ "a"; "b"; "c"; "d" ] = Some "d"
 let%test _ = last [] = None
+
+(* Last Two Elements of a List *)
+(* Alternative syntax:
+   let rec last_two (l : 'a list) : 'a option = match l with *)
+let rec last_two = function
+  | [] | [ _ ] -> None
+  | [ a; b ] -> Some (a, b)
+  | _ :: xs -> last_two xs
+
+let%test _ = last_two [ "a"; "b"; "c"; "d" ] = Some ("c", "d")
+let%test _ = last_two [ "a" ] = None
